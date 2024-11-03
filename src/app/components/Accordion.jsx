5050d0e368312a693bb2a4c6dc0e294018c66296
Troels Agergaard <tras@kea.dk>
2024-11-03 21:58:25 +0100

    Add items with local state func.

---
+++ src/app/components/Accordion.jsx
@@ -0,0 +1,30 @@
+import Item from "./Item";
+const Accordion = () => {
+  return (
+    <div className="relative font-inter antialiased">
+      <main className="relative min-h-screen bg-slate-50 overflow-hidden">
+        <section className="w-full max-w-2xl mx-auto px-4 md:px-6 py-24">
+          <h1 className="text-2xl font-bold text-slate-900 mb-4">FAQs</h1>
+          <Item />
+          <Item />
+          <Item />
+        </section>
+      </main>
+
+      <footer
+        className="absolute left-6 right-6 md:left-12 md:right-auto bottom-4 md:bottom-8 text-center md:text-left"
+        role="contentinfo"
+      >
+        <a
+          className="text-xs text-slate-500 hover:underline"
+          href="https://cruip.com"
+          aria-label="Cruip - Tailwind CSS templates"
+        >
+          &copy; Cruip - Tailwind CSS templates
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Accordion;
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
+import Accordion from "./components/Accordion";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
 }
